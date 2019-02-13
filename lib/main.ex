93ba90eb363d4d8d4b0d902564bc91b6946d9146
Tau <tau@ea7aba.be>
2019-02-13 14:45:48 +0200

    Added the --force flag.
    Github's JSON API at api.github.com is limited to 60 requests per
    hour or so (without authentication). But, access to HTML pages seems
    totally unlimited, so...

--- lib/main.ex
+++ lib/main.ex
@@ -1,16 +1,24 @@
 defmodule NotQuiteAwesome.Main do
   @help """
-  nqa [-h] [--help] [--dry-run] REPOSITORY SECTION
-  
+  nqa [-h] [-f] [--dry-run] REPOSITORY SECTION
+
   NotQuiteAwesome - a parser and sorter of awesome lists on github
                     (https://github.com/sindresorhus/awesome)
-					
+
   At this time it can only sort one section of a list by the number of
   stars (highest first). The SECTION parameter is a regular expression.
-  
+
+  OPTIONS:
+    -h --help
+      show this help message
+    -f --force
+      try to circumvent github API limits
+    --dry-run
+      download and parse the list, but don't follow and sort the links
+
   EXAMPLE: nqa drobakowski/awesome-erlang "Text and Numbers"
   will sort all links under the "Text and Numbers" section by stars.
-  
+
   NOTE:  in order  to  get the  number  of stars  of  a repository  it
   currently relies on  github's JSON API (api.github.com)  which has a
   limit on the  number of requests. You can use  the --dry-run flag in
@@ -19,136 +27,210 @@ defmodule NotQuiteAwesome.Main do
 
   # Runs the do block while displaying the label with a spinner
   defmacro stage(label, do: op) do
-	quote do
-	  do_stage(unquote(label), fn -> unquote(op) end)
-	end
+    quote do
+      do_stage(unquote(label), fn -> unquote(op) end)
+    end
   end
 
   defp do_stage(label, f) do
-	cfg = [frames: :braille, text: label, done: :remove]
-	ProgressBar.render_spinner(cfg, f)
+    cfg = [frames: :braille, text: label, done: :remove]
+    ProgressBar.render_spinner(cfg, f)
   end
-  
-  def main(argv) do
-	Enum.each([:ssl, :inets], &Application.ensure_all_started/1)
 
-	with(
-	  {:ok, repo, section, flags} <- getopts(argv),
-	  {:ok, section} <- section_regexp(section),
-	  {:ok, readme} <- stage("Fetching readme...", do: fetch_readme(repo))
-	) do
-	  repos = stage("Looking for links...", do: find_repo_links(readme, section))
-	  unless flags[:dry_run] do
-		sorted_repos = stage("Sniffing repos...", do: sort_by_stars(repos))
-		format(sorted_repos)
-	  else
-		dry_run(repos)
-	  end
-	else
-	  :help ->
-		IO.write(:stderr, @help)
-	  {:error, msg} ->
-		IO.puts(:stderr, msg)
-	end
+  def main(argv) do
+    Enum.each([:ssl, :inets], &Application.ensure_all_started/1)
+
+    {repo, section, flags} = getopts(argv)
+    section = section_regexp(section)
+    readme = fetch_readme(repo)
+    repos = find_repo_links(readme, section)
+
+    unless flags[:dry_run] do
+      force = flags[:force]
+      unless force do
+        check_api_limit!(Enum.count(repos))
+      end
+      scan_mode = if force, do: :ugly, else: :normal
+      sort_by_stars(repos, scan_mode) |> format()
+    else
+      dry_run(repos)
+    end
+  catch
+    :help ->
+      IO.write(@help)
+    {:abort, msg} ->
+      abort(msg)
+  end
+
+  defp abort(msg) do
+    IO.puts(:stderr, msg)
+    System.halt(1)
   end
 
   defp section_regexp(src) do
-	case Regex.compile(src) do
-	  ok = {:ok, _re} ->
-		ok
-	  {:error, {msg, _}} ->
-		{:error, "Error compiling regular expression: #{msg}"}
-	end
+    case Regex.compile(src) do
+      {:ok, re} ->
+        re
+      {:error, {msg, _}} ->
+        throw {:abort, "Syntax error in SECTION parameter: #{msg}"}
+    end
   end
 
   defp getopts(argv) do
-	cfg = [
-	  strict: [dry_run: :boolean, help: :boolean],
-	  aliases: [h: :help]
-	]
+    cfg = [
+      strict: [
+        dry_run: :boolean,
+        force: :boolean,
+        help: :boolean
+      ],
+      aliases: [h: :help, f: :force]
+    ]
 
-	{flags, argv, invalid} = OptionParser.parse(argv, cfg)
+    {flags, argv, invalid} = OptionParser.parse(argv, cfg)
 
-	unless flags[:help] do
-	  case {argv, invalid} do
-		{[repo, section], []} ->
-		  {:ok, repo, section, flags}
-		_ ->
-		  {:error, "Error: invalid arguments; see --help"}
-	  end
-	else
-	  :help
-	end
+    unless flags[:help] do
+      case {argv, invalid} do
+        {[repo, section], []} ->
+          {repo, section, flags}
+        _ ->
+          throw {:abort, "Error: invalid arguments; see --help"}
+      end
+    else
+      throw :help
+    end
   end
 
   def dry_run(repos) do
-	lines = for {label, repo} <- repos do
-		"\n#{label}: https://github.com/#{repo}"
-	end
+    report = for {label, repo} <- repos do
+        "#{label}: https://github.com/#{repo}\n"
+    end
 
-	IO.puts :stderr, [
-	  "Scanning the following repositories:\n",
-	  lines
-	]
+    IO.write :stderr, [
+      "Scanning the following repositories:\n",
+      "====================================\n",
+      report
+    ]
   end
 
   defp format(repos) do
-	Enum.each repos, fn {label, url, stars} ->
-	  IO.puts("#{label} (#{url}): #{stars}")
-	end
-	if Enum.find(repos, fn {_, _, :error} -> true; _ -> false end) do
-	  IO.puts(:stderr, "WARNING: I were unable to access some repositories (probably due to github API rate limit).")
-	end
+    bad = IO.ANSI.red
+    good = IO.ANSI.green
+    reset = IO.ANSI.reset
+
+    max_prefix_width = repos
+    |> Enum.map(fn({_, _, stars}) -> String.length(to_string(stars)) end)
+    |> Enum.max()
+
+    Enum.each repos, fn {label, url, stars} ->
+      color = if is_number(stars), do: good, else: bad
+      stars = String.pad_leading(to_string(stars), max_prefix_width)
+      IO.puts(" #{color}#{stars}#{reset} #{label} (#{url})")
+    end
+
+    if Enum.find(repos, fn {_, _, rank} -> rank === :error end) do
+      IO.puts :stderr, [
+        "\n", bad, "WARNING: ", reset,
+        "I were unable to access some repositories ",
+        "(probably due to github API rate limit)."
+      ]
+    end
   end
 
-  defp sort_by_stars(repos) do
-	tasks = for {_, repo} <- repos do
-	  Task.async(fn -> fetch_repo_stars(repo) end)
-	end
-	Enum.zip(repos, tasks)
-	|> Enum.map(fn {{label, url}, stars} ->
-	  {label, url, Task.await(stars)}
-	end)
-	|> Enum.sort(fn {_, _, a}, {_, _, b} -> a >= b end)
+  defp sort_by_stars(repos, mode) do
+    stage "Scanning repositories..." do
+      tasks = for {_label, repo} <- repos do
+        Task.async(fn -> fetch_repo_stars(repo, mode) end)
+      end
+
+      Task.yield_many(tasks, 15000)
+      |> Stream.zip(repos)
+      |> Stream.map(fn
+        {{_, {:ok, stars}}, {label, repo}} ->
+          {label, repo, stars}
+        {{_, nil}, {label, repo}} ->
+          {label, repo, :timeout}
+      end)
+      |> Enum.sort_by(&elem(&1, 2), &>=/2)
+    end
   end
 
-  defp fetch_repo_stars(repo) do
-	try do
-	  Map.get(fetch_repo_info(repo), "stargazers_count")
-	catch
-	  %HTTP.RequestError{} ->
-		:error
-	end
+  defp fetch_repo_stars(repo, mode) do
+    case mode do
+      :normal ->
+        HTTP.get('https://api.github.com/repos/#{repo}')
+        |> to_string()
+        |> JSON.decode!()
+        |> Map.get("stargazers_count")
+      :ugly ->
+        page = HTTP.get('https://github.com/#{repo}') |> to_string()
+        case Regex.run(~r<(\d+) users? starred this>, page) do
+          [_, stars] ->
+            String.to_integer(stars)
+          _ ->
+            :error
+        end
+    end
+  catch
+    %HTTP.RequestError{} ->
+      :error
   end
 
-  defp fetch_repo_info(repo) do
-	HTTP.get('https://api.github.com/repos/' ++ to_charlist(repo))
-	|> to_string()
-	|> JSON.decode!()
+  defp check_api_limit!(minimum) do
+    limits = stage "Checking API limits...", do: fetch_api_limits()
+    rem = limits.remaining
+    if minimum > rem do
+      message = [
+        "Github API limit exceeded: #{minimum} calls required, got only #{rem}\n",
+        "Next limits reset at ", format_timestamp(limits.reset),
+        "\nYou can use the --force flag to try and circumvent this."
+      ]
+      throw {:abort, message}
+    end
+  end
+
+  defp format_timestamp(t) do
+    lt = :calendar.system_time_to_local_time(t, :second)
+    {{yy, mm, dd}, {h, m, _s}} = lt
+    :io_lib.format('~b-~2..0b-~2..0b ~2..0b:~2..0b', [yy, mm, dd, h, m])
+  end
+
+  defp fetch_api_limits() do
+    HTTP.get('https://api.github.com/rate_limit')
+    |> to_string()
+    |> JSON.decode!()
+    |> get_in(["resources", "core"])
+    |> Enum.reduce(%{}, fn
+      {k, v}, acc when k in ~w[remaining reset] and is_number(v) ->
+        Map.put(acc, String.to_atom(k), v)
+      _, acc ->
+        acc
+    end)
   end
 
   defp fetch_readme(repo) do
-	variants = ["README.md", "readme.md", "README.MD"]
-	try do
-	  Enum.each(variants, fn v -> try_readme(repo, v) end)
-	catch
-	  r = {:ok, _tree} -> r
-	else
-	  _ -> {:error, "No README file found"}
-	end
+    readme = stage "Fetching README..." do
+      variants = ["README.md", "readme.md", "README.MD"]
+      try_readmes(repo, variants)
+    end
+
+    if is_nil(readme) do
+      throw {:abort, "No README file found"}
+    else
+      readme
+    end
   end
 
-  defp try_readme(repo, variant) do
-	try do
-	  HTTP.get('https://raw.githubusercontent.com/#{repo}/master/#{variant}')
-	  |> to_string()
-	  |> Earmark.parse(%Earmark.Options{smartypants: false})
-	  |> fn {tree, _} -> {:ok, tree} end.()
-	catch
-	  %HTTP.RequestError{} -> :ignore
-	else
-	  result -> throw result
-	end
+  defp try_readmes(_repo, []), do: nil
+  defp try_readmes(repo, [variant | rest]) do
+    HTTP.get('https://raw.githubusercontent.com/#{repo}/master/#{variant}')
+    |> to_string()
+    |> Earmark.parse(%Earmark.Options{smartypants: false})
+  catch
+    %HTTP.RequestError{} ->
+      try_readmes(repo, rest)
+  else
+    {tree, _} ->
+      tree
   end
 
   alias Earmark.Block.Heading, as: MdH
@@ -156,29 +238,29 @@ defmodule NotQuiteAwesome.Main do
   alias Earmark.Block.List, as: MdL
   alias Earmark.Block.ListItem, as: MdLI
   defp find_repo_links(tree, section) do
-	block = Enum.reduce tree, {false, []}, fn
-	  (h = %MdH{}, {_, acc}) -> {Regex.match?(section, h.content), acc};
-	  (_, {false, acc}) -> {false, acc};
-	  (l, {true, acc}) -> {true, [scavenge_links(l) | acc]}
-	end
-	{_, block} = block
-	List.flatten(block)
+    block = Enum.reduce tree, {false, []}, fn
+      (h = %MdH{}, {_, acc}) -> {Regex.match?(section, h.content), acc};
+      (_, {false, acc}) -> {false, acc};
+      (l, {true, acc}) -> {true, [scavenge_links(l) | acc]}
+    end
+    {_, block} = block
+    List.flatten(block)
   end
 
   defp scavenge_links(%MdL{blocks: list}) do
-	for item <- list, do: scavenge_links(item)
+    for item <- list, do: scavenge_links(item)
   end
   defp scavenge_links(%MdLI{blocks: list}) do
-	for item <- list, do: scavenge_links(item)
+    for item <- list, do: scavenge_links(item)
   end
   defp scavenge_links(%MdP{lines: lines}) do
-	links = for line <- lines, do: scavenge_links(line)
-	Enum.filter(links, fn x -> !is_nil(x) end)
+    links = for line <- lines, do: scavenge_links(line)
+    Enum.filter(links, fn x -> !is_nil(x) end)
   end
   defp scavenge_links(line) do
-	case Regex.run(~r<\[([^\]]*)\]\(https://github.com/([^()/#?]*)/([^()/#?]*)[^()]*\)>, line) do
-	  [_, label, user, repo | _] -> {label, "#{user}/#{repo}"}
-	  _ -> nil
-	end
+    case Regex.run(~r<\[([^\]]*)\]\(https://github.com/([^()/#?]*)/([^()/#?]*)[^()]*\)>, line) do
+      [_, label, user, repo | _] -> {label, "#{user}/#{repo}"}
+      _ -> nil
+    end
   end
 end
